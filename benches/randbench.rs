d6bfd5def3faa32a6b6406c754a2dc7fc04fc2f8
aspen <luxx4x@protonmail.com>
2020-09-14 06:19:41 +0000

    Update readme; add comparisons to benchmark

--- benches/randbench.rs
+++ benches/randbench.rs
@@ -1,5 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use nanorand::rand::RNG;
+use rand_core::RngCore;
+use random_fast_rng::Random;
 
 fn criterion_benchmark(c: &mut Criterion) {
 	let mut entropy_group = c.benchmark_group("entropy");
@@ -79,6 +81,88 @@ fn criterion_benchmark(c: &mut Criterion) {
 	}
 
 	rng_group.finish();
+
+	let mut other_rngs = c.benchmark_group("other-rng-crates");
+	other_rngs.throughput(Throughput::Bytes(std::mem::size_of::<u64>() as u64 * 1024));
+
+	other_rngs.bench_function("oorandom pcg32", |b| {
+		let mut rng = oorandom::Rand32::new(42);
+		b.iter(|| {
+			let mut n: u32 = u32::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.rand_u32());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.bench_function("oorandom pcg64", |b| {
+		let mut rng = oorandom::Rand64::new(42);
+		b.iter(|| {
+			let mut n: u64 = u64::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.rand_u64());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.bench_function("randomize pcg32", |b| {
+		let mut rng = randomize::PCG32::seed(42, 0);
+		b.iter(|| {
+			let mut n: u32 = u32::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.next_u32());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.bench_function("randomize pcg64", |b| {
+		let mut rng = randomize::PCG64::seed(42, 0);
+		b.iter(|| {
+			let mut n: u64 = u64::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.next_u64());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.bench_function("rand pcg32", |b| {
+		let mut rng = rand_pcg::Pcg32::new(0xcafef00dd15ea5e5, 0xa02bdbf7bb3c0a7);
+		b.iter(|| {
+			let mut n: u32 = u32::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.next_u32());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.bench_function("rand pcg64", |b| {
+		let mut rng = rand_pcg::Pcg64::new(0xcafef00dd15ea5e5, 0xa02bdbf7bb3c0a7ac28fa16a64abf96);
+		b.iter(|| {
+			let mut n: u64 = u64::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.next_u64());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.bench_function("random-fast-rng pcg32", |b| {
+		let mut rng = random_fast_rng::FastRng::new();
+		b.iter(|| {
+			let mut n: u64 = u64::MIN;
+			for _ in 0..1024 {
+				n = n.wrapping_add(rng.gen());
+			}
+			black_box(n);
+		})
+	});
+
+	other_rngs.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
