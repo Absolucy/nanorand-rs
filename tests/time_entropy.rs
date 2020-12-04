929b2f25fa1abca6ccc66500fd32c26ff7d33968
aspen <aspenuwu@protonmail.com>
2020-12-04 16:59:40 -0500

    test: Fix tests not compiling

--- tests/time_entropy.rs
+++ tests/time_entropy.rs
@@ -2,17 +2,19 @@ use nanorand::entropy::emergency_system_time_entropy;
 
 #[test]
 fn system_entropy_test() {
-	let test_entropy = emergency_system_time_entropy(16);
-	println!("16 bytes of entropy: {}", hex::encode(test_entropy));
+	let mut test_entropy_out = [0u8; 16];
+	emergency_system_time_entropy(&mut test_entropy_out);
+	println!("16 bytes of entropy: {}", hex::encode(test_entropy_out));
 
-	let test_entropy_b = emergency_system_time_entropy(std::mem::size_of::<u64>() * 64);
-	let unrandom = test_entropy_b
+	let mut test_entropy_b_out = [0u8; std::mem::size_of::<u64>() * 6];
+	emergency_system_time_entropy(&mut test_entropy_b_out);
+	let unrandom = test_entropy_b_out
 		.iter()
-		.fold(0, |a, number| if *number == 42 { a + 1 } else { a });
+		.fold(0, |a, number| if *number == 0 { a + 1 } else { a });
 	println!(
-		"{} bytes of entropy has {} instances of '42'",
+		"{} bytes of entropy has {} instances of '0'",
 		std::mem::size_of::<u64>() * 64,
 		unrandom
 	);
-	assert_ne!(test_entropy_b, vec![42; std::mem::size_of::<u64>() * 64])
+	assert_ne!(test_entropy_b_out, [0u8; std::mem::size_of::<u64>() * 6])
 }
