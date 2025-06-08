f4154607d291024097a306f6ae4f407b9a24efd9
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2025-06-08 06:42:47 +0200

    fix signature of RtlGenRandom and update entropy correspondingly (#53)

--- src/entropy/windows.rs
+++ src/entropy/windows.rs
@@ -1,9 +1,11 @@
+use core::ffi::{c_void, c_ulong, c_char};
+
 extern "system" {
 	#[link_name = "SystemFunction036"]
-	fn RtlGenRandom(pBuffer: *mut u8, cbBuffer: usize) -> u32;
+	fn RtlGenRandom(pBuffer: *mut c_void, cbBuffer: c_ulong) -> c_char;
 }
 
 /// Obtain a random 64-bit number using WinAPI's `RtlGenRandom` function.
 pub fn entropy(out: &mut [u8]) -> bool {
-	unsafe { RtlGenRandom(out.as_mut_ptr(), out.len()) == 0 }
+	unsafe { RtlGenRandom(out.as_mut_ptr() as *mut c_void, out.len() as u32) == 0 }
 }
